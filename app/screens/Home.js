ffaa7edbc11b93a21650fc6f0731d74315e1d3ac
saki <16546851+sakiko-2@users.noreply.github.com>
2019-10-20 00:08:15 +1300

    handle number input, format display

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -6,6 +6,50 @@ import Button from '../components/Button';
 import ButtonRow from '../components/ButtonRow';
 
 class Home extends Component {
+  state = {
+    current: '0'
+  };
+
+  handlePress(value) {
+    if (typeof value === 'number') {
+      this.handleNumber(value);
+    }
+    else if (typeof value === 'string') {
+      this.handleOperator(value);
+    }
+  };
+
+  handleNumber(num) {
+    this.setState(state => {
+      if (state.current === '0') {
+        return { current: `${num}` }
+      }
+      else {
+        return { current: `${state.current}${num}` }
+      }
+    });
+  };
+
+  handleOperator(str) {
+    switch (str) {
+      case 'c':
+        this.setState({ current: '0' });
+      case '+/-':
+      case '%':
+      case '/':
+      case '*':
+      case '-':
+      case '+':
+      case '.':
+      case '=':
+        break;
+    }
+  };
+
+  formatNumber(num) {
+    return parseInt(num).toLocaleString()
+  };
+
   render() {
     const darkColor = '#444';
     const lightColor = '#9e9e9e';
@@ -16,35 +60,35 @@ class Home extends Component {
     return (
       <SafeAreaView>
         <View style={styles.container}>
-          <Display text={'1,000'}/>
+          <Display text={this.formatNumber(this.state.current)}/>
           <ButtonRow>
-            <Button text={'C'} color={darkText} backgroundColor={lightColor} onPress={() => alert('pressed')} />
-            <Button text={'+/-'} color={darkText} backgroundColor={lightColor} onPress={() => alert('pressed')} />
-            <Button text={'%'} color={darkText} backgroundColor={lightColor} onPress={() => alert('pressed')} />
-            <Button text={'÷'} color={lightText} backgroundColor={accentColor} onPress={() => alert('pressed')} />
+            <Button text={'C'} color={darkText} backgroundColor={lightColor} onPress={() => this.handlePress('c')} />
+            <Button text={'+/-'} color={darkText} backgroundColor={lightColor} onPress={() => this.handlePress('+/-')} />
+            <Button text={'%'} color={darkText} backgroundColor={lightColor} onPress={() => this.handlePress('%')} />
+            <Button text={'÷'} color={lightText} backgroundColor={accentColor} onPress={() => this.handlePress('/')} />
           </ButtonRow>
           <ButtonRow>
-            <Button text={'7'} color={lightText} backgroundColor={darkColor} onPress={() => alert('pressed')} />
-            <Button text={'8'} color={lightText} backgroundColor={darkColor} onPress={() => alert('pressed')} />
-            <Button text={'9'} color={lightText} backgroundColor={darkColor} onPress={() => alert('pressed')} />
-            <Button text={'x'} color={lightText} backgroundColor={accentColor} onPress={() => alert('pressed')} />
+            <Button text={'7'} color={lightText} backgroundColor={darkColor} onPress={() => this.handlePress(7)} />
+            <Button text={'8'} color={lightText} backgroundColor={darkColor} onPress={() => this.handlePress(8)} />
+            <Button text={'9'} color={lightText} backgroundColor={darkColor} onPress={() => this.handlePress(9)} />
+            <Button text={'x'} color={lightText} backgroundColor={accentColor} onPress={() => this.handlePress('*')} />
           </ButtonRow>
           <ButtonRow>
-            <Button text={'4'} color={lightText} backgroundColor={darkColor} onPress={() => alert('pressed')} />
-            <Button text={'5'} color={lightText} backgroundColor={darkColor} onPress={() => alert('pressed')} />
-            <Button text={'6'} color={lightText} backgroundColor={darkColor} onPress={() => alert('pressed')} />
-            <Button text={'-'} color={lightText} backgroundColor={accentColor} onPress={() => alert('pressed')} />
+            <Button text={'4'} color={lightText} backgroundColor={darkColor} onPress={() => this.handlePress(4)} />
+            <Button text={'5'} color={lightText} backgroundColor={darkColor} onPress={() => this.handlePress(5)} />
+            <Button text={'6'} color={lightText} backgroundColor={darkColor} onPress={() => this.handlePress(6)} />
+            <Button text={'-'} color={lightText} backgroundColor={accentColor} onPress={() => this.handlePress('-')} />
           </ButtonRow>
           <ButtonRow>
-            <Button text={'1'} color={lightText} backgroundColor={darkColor} onPress={() => alert('pressed')} />
-            <Button text={'2'} color={lightText} backgroundColor={darkColor} onPress={() => alert('pressed')} />
-            <Button text={'3'} color={lightText} backgroundColor={darkColor} onPress={() => alert('pressed')} />
-            <Button text={'+'} color={lightText} backgroundColor={accentColor} onPress={() => alert('pressed')} />
+            <Button text={'1'} color={lightText} backgroundColor={darkColor} onPress={() => this.handlePress(1)} />
+            <Button text={'2'} color={lightText} backgroundColor={darkColor} onPress={() => this.handlePress(2)} />
+            <Button text={'3'} color={lightText} backgroundColor={darkColor} onPress={() => this.handlePress(3)} />
+            <Button text={'+'} color={lightText} backgroundColor={accentColor} onPress={() => this.handlePress('+')} />
           </ButtonRow>
           <ButtonRow>
-            <Button text={'0'} color={lightText} backgroundColor={darkColor} wide='true' onPress={() => alert('pressed')} />
-            <Button text={'.'} color={lightText} backgroundColor={darkColor} onPress={() => alert('pressed')} />
-            <Button text={'='} color={lightText} backgroundColor={darkColor} onPress={() => alert('pressed')} />
+            <Button text={'0'} color={lightText} backgroundColor={darkColor} wide='true' onPress={() => this.handlePress(0)} />
+            <Button text={'.'} color={lightText} backgroundColor={darkColor} onPress={() => this.handlePress('.')} />
+            <Button text={'='} color={lightText} backgroundColor={darkColor} onPress={() => this.handlePress('=')} />
           </ButtonRow>
         </View>
       </SafeAreaView>
